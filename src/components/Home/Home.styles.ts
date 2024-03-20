1e67be5ed1e7d6c698c08e864f24c454fbfb2cb2
Pinto Manuel <pintoydc@gmail.com>
2024-03-20 05:41:21 +0200

    Bg image is added

--- src/components/Home/Home.styles.ts
+++ src/components/Home/Home.styles.ts
@@ -6,4 +6,12 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    blueOverlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 255, 0.5)', 
+    }
 });
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
-
+import React from "react";
+import { View, ImageBackground } from "react-native";
+import styles from "./Home.styles";
 
 interface HomeProps {
-    userId: string;
+  userId: string;
+}
 
-  }
-  
-  const Home: React.FC<HomeProps> = (userId) => {
-    console.log(userId);
-    return (
-        <View>
-       
-        </View>
-    );
+const Home: React.FC<HomeProps> = (userId) => {
+  console.log(userId);
+
+  return (
+    <ImageBackground
+      source={require("../../../assets/home.jpg")}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.blueOverlay} />
+      <View></View>
+    </ImageBackground>
+  );
 };
 
 export default React.memo(Home);
