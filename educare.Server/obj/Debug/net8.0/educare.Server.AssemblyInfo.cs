f321b4de96aa0e8a81cb081f3e309ba5d89ba503
Pinto Manuel <pintoydc@gmail.com>
2024-03-28 01:30:26 +0200

    Commit code

--- educare.Server/obj/Debug/net8.0/educare.Server.AssemblyInfo.cs
+++ educare.Server/obj/Debug/net8.0/educare.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("educare.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179ecfff4330561a20a837a7b0775f116a173d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8933637130d45e1cd5024f8d1db62385687df121")]
 [assembly: System.Reflection.AssemblyProductAttribute("educare.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("educare.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
